3002c5bcb6d3548111caa38984f6a9709bfa7f38
Dmitry Sorokin (@sorydima) <43252008+sorydima@users.noreply.github.com>
2024-12-23 00:33:50 +0300

    Initial release ..

---
+++ android/app/src/main/kotlin/pro/natalee/nastasia/MainActivity.kt
@@ -0,0 +1,6 @@
+package pro.natalee.nastasia
+
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
+class MainActivity: FlutterFragmentActivity() {
+}
